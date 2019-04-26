dee39f695372621631e5cfcf57a7a5ca58eac15e
IvanovEvgeniy <yakudZZZa@list.ru>
2019-04-26 14:35:29 +0500

    Adding touch processing and game logic

--- Ping-Pong_Game/GameViewController.m
+++ Ping-Pong_Game/GameViewController.m
@@ -32,6 +32,8 @@
 @end
 
 @implementation GameViewController
+    
+#pragma mark - Подготовка UI
 
 - (void)prepareUI {
     
@@ -72,10 +74,201 @@
     [self.view addSubview:_scoreBottom];
     
 }
+    
+#pragma mark - Логика игры
+    
+- (void)stop {
+    if (_timer) {
+        [_timer invalidate];
+        _timer = nil;
+    }
+    _ball.hidden = YES;
+}
+    
+- (void)newGame {
+    [self reset];
+    
+    _scoreTop.text = @"0";
+    _scoreBottom.text = @"0";
+    
+    [self displayMessage:@"Готовы к игре?"];
+}
+    
+- (void)start {
+    _ball.center = CGPointMake(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT);
+    if (!_timer) {
+        _timer = [NSTimer scheduledTimerWithTimeInterval:1.0/60.0 target:self selector:@selector(animate) userInfo:nil repeats:YES];
+    }
+    _ball.hidden = NO;
+}
+    
+- (void)displayMessage:(NSString *)message {
+    [self stop];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Ping Pong" message:message preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+        if ([self gameOver]) {
+            [self newGame];
+            return;
+        }
+        [self reset];
+        [self start];
+    }];
+    [alertController addAction:action];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+    
+- (int)gameOver {
+    if ([_scoreTop.text intValue] >= MAX_SCORE) return 1;
+    if ([_scoreBottom.text intValue] >= MAX_SCORE) return 2;
+    return 0;
+}
+    
+- (void)reset {
+    if ((arc4random() % 2) == 0) {
+        _dx = -1;
+    } else {
+        _dx = 1;
+    }
+    
+    if (_dy != 0) {
+        _dy = -_dy;
+    } else if ((arc4random() % 2) == 0) {
+        _dy = -1;
+    } else  {
+        _dy = 1;
+    }
+    
+    _ball.center = CGPointMake(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT);
+    
+    _speed = 2;
+}
+    
+- (void)animate {
+    
+    if (_paddleTop.center.x < _ball.center.x) {
+        _paddleTop.center = CGPointMake(_paddleTop.center.x + _speed * 0.9, 40+_paddleTop.bounds.size.height/2);
+    } else if (_paddleTop.center.x > _ball.center.x) {
+        _paddleTop.center = CGPointMake(_paddleTop.center.x - _speed * 0.9, 40+_paddleTop.bounds.size.height/2);
+    } else {
+        _paddleTop.center = CGPointMake(_paddleTop.center.x, 40+_paddleTop.bounds.size.height/2);
+    }
+    
+    _ball.center = CGPointMake(_ball.center.x + _dx*_speed, _ball.center.y + _dy*_speed);
+    [self checkCollision:CGRectMake(-20, 0, 20, SCREEN_HEIGHT) X:fabs(_dx) Y:0];
+    [self checkCollision:CGRectMake(SCREEN_WIDTH, 0, 20, SCREEN_HEIGHT) X:-fabs(_dx) Y:0];
+    if ([self checkCollision:_paddleTop.frame X:(_ball.center.x - _paddleTop.center.x) / 32.0 Y:1]) {
+        [self increaseSpeed];
+    }
+    if ([self checkCollision:_paddleBottom.frame X:(_ball.center.x - _paddleBottom.center.x) / 32.0 Y:-1]) {
+        [self increaseSpeed];
+    }
+    [self goal];
+}
+    
+- (void)increaseSpeed {
+    _speed += 0.5;
+    if (_speed > 10) _speed = 10;
+}
+    
+- (BOOL)checkCollision: (CGRect)rect X:(float)x Y:(float)y {
+    if (CGRectIntersectsRect(_ball.frame, rect)) {
+        if (x != 0) _dx = x;
+        if (y != 0) _dy = y;
+        return YES;
+    }
+    return NO;
+}
+    
+- (BOOL)goal
+    {
+        if (_ball.center.y < 0 || _ball.center.y >= SCREEN_HEIGHT) {
+            int s1 = [_scoreTop.text intValue];
+            int s2 = [_scoreBottom.text intValue];
+            
+            if (_ball.center.y < 0) ++s2; else ++s1;
+            _scoreTop.text = [NSString stringWithFormat:@"%u", s1];
+            _scoreBottom.text = [NSString stringWithFormat:@"%u", s2];
+            
+            int gameOver = [self gameOver];
+            if (gameOver) {
+                [self displayMessage:[NSString stringWithFormat:@"Игрок %i выиграл", gameOver]];
+            } else {
+                [self reset];
+            }
+            
+            return YES;
+        }
+        return NO;
+    }
+    
+#pragma mark - Обработка нажатий
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        CGPoint point = [touch locationInView:self.view];
+        if (_bottomTouch == nil && point.y > HALF_SCREEN_HEIGHT) {
+            _bottomTouch = touch;
+            _paddleBottom.center = CGPointMake(point.x, point.y);
+        }
+        else if (_topTouch == nil && point.y < HALF_SCREEN_HEIGHT) {
+            _topTouch = touch;
+            _paddleTop.center = CGPointMake(point.x, point.y);
+        }
+    }
+}
+    
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        CGPoint point = [touch locationInView:self.view];
+        if (touch == _topTouch) {
+            if (point.y > HALF_SCREEN_HEIGHT) {
+                _paddleTop.center = CGPointMake(point.x, HALF_SCREEN_HEIGHT);
+                return;
+            }
+            _paddleTop.center = point;
+        }
+        else if (touch == _bottomTouch) {
+            if (point.y < HALF_SCREEN_HEIGHT) {
+                _paddleBottom.center = CGPointMake(point.x, HALF_SCREEN_HEIGHT);
+                return;
+            }
+            _paddleBottom.center = point;
+        }
+    }
+}
+    
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        if (touch == _topTouch) {
+            _topTouch = nil;
+        }
+        else if (touch == _bottomTouch) {
+            _bottomTouch = nil;
+        }
+    }
+}
+    
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self touchesEnded:touches withEvent:event];
+}
+    
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self prepareUI];
+}
+    
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self becomeFirstResponder];
+    [self newGame];
+}
+    
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [self resignFirstResponder];
 }
 
 
